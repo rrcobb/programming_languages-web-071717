8b4786a12d372755d15c361be99fbe2a78410956
Rob Cobb <rwcobbjr@gmail.com>
2018-02-25 15:37:26 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
-  # your code here
+  languages.reduce(Hash.new { |hash, key| hash[key] = {}} ) do |memo, (type, languages)|
+    languages.each do |lang, info|
+      memo[lang].merge!(info)
+      memo[lang][:style] ||= []
+      memo[lang][:style].push(type)
+    end
+    memo
+  end
 end
